68cb0d6139396f1736fd707dc24bc7fc686579dc
Gifftybabe <ulokangozi@gmail.com>
2023-04-24 12:34:42 -0700

    Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""Exports to-do list information for a given employee ID to CSV format.""""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
